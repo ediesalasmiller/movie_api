6a0727173ebe2fd01146f478b697644a6ae309b0
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-03-16 15:04:14 -0700

    updated comments

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 
 const app = express();
 
+//firing middleware, common in parameter logs basic data
 app.use(morgan('common'));
 
+// GET requests
+//json because an API will be here eventually.
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
@@ -13,8 +16,10 @@ app.get('/', (req, res) => {
     res.send('These are the top 10 movies!');
 });
 
+// serving all files in the public folder instead of res.sendFile() function over and over.
 app.use(express.static('public'));
 
+//error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
