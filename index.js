2434e11839fcad52e06a556f3a14952303da3660
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-03-16 15:00:01 -0700

    added error handler

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require('express');
+    morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('These are the top 10 movies!');
+});
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
