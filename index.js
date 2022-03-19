e50eada544d8094635900910149a9c18db4c8063
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-03-18 20:17:06 -0700

    Added in memory array

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const express = require('express');
     morgan = require('morgan');
+    bodyParser = require('body-parser');
+    uuid = require('uuid');
 
 const app = express();
 
 //firing middleware, common in parameter logs basic data
 app.use(morgan('common'));
+//for client adding new info: body parser allows you to read body of HTTP requests within request hanglers by using-> req.body
+app.use(bodyParser.json());
 
 let topMovies = [
     {
-        title: 'Good Will Hunting'
+        title: 'Good Will Hunting',
+        director: 'Gus Van Sant',
+        genre: 'drama',
+        description: 'Will Hunting has a genius-level IQ but chooses to work as a janitor at MIT. When he solves a difficult graduate-level math problem, his talents are discovered by a professor, who decides to help the misguided youth reach his potential. When Will is arrested for attacking a police officer, the professor makes a deal to get leniency for him if he will get treatment from therapist Sean Maguire.'
     },
     {
-        title: 'Sabrina'
+        title: 'Sabrina',
+        director: 'Billy Wilder',
+        genre: 'drama-comedy',
+        description: 'A playboy becomes interested in the daughter of his family chauffeur, but it is his more serious brother who would be the better man for her.'
     },
     {
-        title: 'Forrest Gump'
+        title: 'Forrest Gump',
+        director: 'Robert Zemeckis',
+        genre: 'drama-comedy',
+        description: 'Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.'
     },
+    {
+        title: 'The Darjeeling Limited',
+        director: 'Wes Anderson',
+        genre: 'indie',
+        description: 'A year after the death of their father, three brothers attempt to bond with each other by traveling together by train across India.'
+    },
+    {
+        title: 'Roman Holiday',
+        director: 'William Wyler',
+        genre: 'romance-comedy',
+        description: 'A runaway princess in Rome finds love with a reporter who knows her true identity.'
+    },
+    
 ]
 // GET requests
 //stand in
