286fc4bd6cd3444a35d056e3f321377a72bfa4bc
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-04-04 13:55:40 -0700

    Remove node_modules folder

--- index.js
+++ index.js
@@ -69,7 +69,20 @@ app.get('/users', (req, res) => {
         res.status(500).send('Error: ' + err);
     });
 });
-// GET ALL USERS
+
+//get user by username
+app.get('/users:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+
 app.get('/', (req, res) => {
     res.send('My flixdB');
 });
