0505b1acaea0e76d17ad05994483fa5ac913dae2
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-04-12 15:30:57 -0700

    environmental variable in heroku

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI , { useNewUrlParser: true, useUnifiedTopology: true });
     
 
 app.use(morgan('common')); //firing middleware, common in parameter logs basic data
